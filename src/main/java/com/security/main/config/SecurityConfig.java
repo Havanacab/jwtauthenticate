3a4f3e3bafa28409503f13e6ef8e542968f33635
Havanacab <nepathya0608@gmail.com>
2023-10-03 16:52:06 -0700

    first jwt commit

--- src/main/java/com/security/main/config/SecurityConfig.java
+++ src/main/java/com/security/main/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.security.main.config;
 
+import com.security.main.filter.JwtAuthFilter;
 import com.security.main.service.UserInfoUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,11 +20,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter authFilter;
     @Bean
     UserDetailsService userDetailsService(){
        /* UserDetails admin = User.withUsername("Sonam")
@@ -39,12 +48,17 @@ public class SecurityConfig {
 
        return  http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/products/mypage", "/products/new").permitAll()
+                .requestMatchers("/products/mypage", "/products/new", "/products/authenticate").permitAll()
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/products/**").authenticated()
-                .and().formLogin()
-                .and().build();
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+               .and()
+               .authenticationProvider(authenticationProvider())
+               .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+               .build();
     }
 
 
@@ -58,5 +72,9 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/security/main/controller/ProductController.java
+++ src/main/java/com/security/main/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.security.main.controller;
 
+import com.security.main.dto.AuthRequest;
 import com.security.main.dto.Product;
 import com.security.main.entity.UserInfo;
+import com.security.main.service.JwtService;
 import com.security.main.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,32 +22,50 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @GetMapping("/mypage")
-    public String getMe(){
+    public String getMe() {
 
         return "HELLO! WELCOME TO MY WEBSITE..";
     }
 
     @GetMapping("/all")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
 
         return service.getProducts();
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('ROLE_USER')")
-    public Product getById(@PathVariable int id){
+    public Product getById(@PathVariable int id) {
 
         return service.getProduct(id);
     }
 
     @PostMapping("/new")
-    public String addUser(@RequestBody UserInfo userInfo){
+    public String addUser(@RequestBody UserInfo userInfo) {
 
-       return service.addUser(userInfo);
+        return service.addUser(userInfo);
 
     }
 
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
 
+        if (authentication.isAuthenticated()) {
+
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request..");
+        }
+
+
+    }
 }
